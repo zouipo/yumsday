9560a8d68d89a96ae254b233cf3536bf75e8b446
popobg <pauline.bouyssou1@gmail.com>
2026-02-04 18:36:03 +0100

    Adapt database creation script

--- backend/data/migrations/0_db-creation.sql
+++ backend/data/migrations/0_db-creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS user_group (
     id INTEGER PRIMARY KEY NOT NULL UNIQUE,
     name VARCHAR NOT NULL,
     image_url VARCHAR,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL
 );
 
 -- USER
@@ -12,38 +12,38 @@ CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY NOT NULL UNIQUE,
     username VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
-    app_admin BOOLEAN DEFAULT FALSE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    app_admin BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     avatar VARCHAR,
-    language VARCHAR DEFAULT 'en' NOT NULL,
-    app_theme VARCHAR DEFAULT 'light' NOT NULL,
+    language VARCHAR NOT NULL,
+    app_theme VARCHAR NOT NULL,
     last_visited_group INTEGER,
     FOREIGN KEY (last_visited_group) REFERENCES user_group(id)
+);
+
+-- MEMBER (Many-to-Many relationship between USER and USER_GROUP)
+-- Stores the admin flag for admins of the group
+CREATE TABLE IF NOT EXISTS member_group (
+    user_id INTEGER NOT NULL,
+    user_group_id INTEGER NOT NULL,
+    admin BOOLEAN NOT NULL,
+    joined_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id, user_group_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (user_group_id) REFERENCES user_group(id)
 );
 
 -- SESSION (manage cookies sessions)
 CREATE TABLE IF NOT EXISTS session (
     id INTEGER PRIMARY KEY NOT NULL UNIQUE,
     session_id VARCHAR NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL,
+    last_activity TIMESTAMP NOT NULL,
     ip_address VARCHAR NOT NULL,
-    last_activity TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     user_agent VARCHAR,
     user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id)
-);
-
--- MEMBER (Many-to-Many relationship between USER and USER_GROUP)
--- Stores the admin flag to, for admins of the group
-CREATE TABLE IF NOT EXISTS member_group (
-    user_id INTEGER NOT NULL,
-    user_group_id INTEGER NOT NULL,
-    admin BOOLEAN DEFAULT FALSE NOT NULL,
-    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (user_id, user_group_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (user_group_id) REFERENCES user_group(id)
 );
 
 -- UNIT
@@ -69,18 +69,34 @@ CREATE TABLE IF NOT EXISTS recipe (
     id INTEGER PRIMARY KEY NOT NULL UNIQUE,
     name VARCHAR NOT NULL,
     description VARCHAR,
-    recipe_category VARCHAR,
     image_url VARCHAR,
     original_link VARCHAR,
     preparation_time INTEGER,
     cooking_time INTEGER,
     servings INTEGER,
     instructions VARCHAR,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    public BOOLEAN DEFAULT FALSE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    public BOOLEAN NOT NULL,
     comment VARCHAR,
     user_group_id INTEGER NOT NULL,
     FOREIGN KEY (user_group_id) REFERENCES user_group(id)
+);
+
+-- CATEGORY
+CREATE TABLE IF NOT EXISTS category (
+    id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+    name VARCHAR NOT NULL,
+    user_group_id INTEGER NOT NULL,
+    FOREIGN KEY (user_group_id) REFERENCES user_group(id)
+);
+
+-- RECIPE_CATEGORY (Many-to-Many relationship between RECIPE and CATEGORY)
+CREATE TABLE IF NOT EXISTS recipe_category (
+    recipe_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    PRIMARY KEY (recipe_id, category_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 -- INGREDIENT
