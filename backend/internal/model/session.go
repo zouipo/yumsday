6cba7a894bdc99ba935694b173ad840c7863d02a
popobg <pauline.bouyssou1@gmail.com>
2026-03-01 22:54:53 +0100

    Add session model and update db creation

---
+++ backend/internal/model/session.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"time"
+
+	"github.com/zouipo/yumsday/backend/internal/pkg/utils"
+)
+
+type Session struct {
+	ID           string    `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastActivity time.Time `json:"last_activity"`
+	IPAddress    string    `json:"ip_address"`
+	UserAgent    string    `json:"user_agent"`
+	UserID       string    `json:"user_id"`
+}
+
+// NewSession creates a new session with a unique session ID and sets the creation and expiration times.
+func NewSession() *Session {
+	return &Session{
+		ID:           utils.GenerateSessionID(),
+		CreatedAt:    time.Now().UTC(),
+		LastActivity: time.Now().UTC(),
+	}
+}
