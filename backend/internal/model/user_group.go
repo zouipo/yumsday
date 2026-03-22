35929091586c3bc9e4f1ceeea76b57a331edd72c
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-03-22 18:47:25 +0100

    image url nullage in UserGroup

--- backend/internal/model/user_group.go
+++ backend/internal/model/user_group.go
@@ -5,6 +5,6 @@ import "time"
 type UserGroup struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
-	ImageURL  string    `json:"image_url"`
+	ImageURL  *string   `json:"image_url"`
 	CreatedAt time.Time `json:"created_at"`
 }
