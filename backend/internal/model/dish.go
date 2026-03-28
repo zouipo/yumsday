4963b41ad7bd0c715844bea84fb69be357cfa3f6
popobg <pauline.bouyssou1@gmail.com>
2026-03-28 18:47:56 +0100

    Update model to new ER diagram

--- backend/internal/model/dish.go
+++ backend/internal/model/dish.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Dish struct {
-	ID          int64     `json:"id"`
-	Portion     int       `json:"portion"`
-	Bought      bool      `json:"bought"`
-	Datetime    time.Time `json:"datetime"`
-	UserGroupID int64     `json:"user_group_id"`
-	RecipeID    int64     `json:"recipe_id"`
+	ID       int64     `json:"id"`
+	Portion  int       `json:"portion"`
+	Bought   bool      `json:"bought"`
+	Datetime time.Time `json:"datetime"`
+	GroupID  int64     `json:"group_id"`
+	Recipes  []Recipe  `json:"recipes"`
 }
--- backend/internal/model/item.go
+++ backend/internal/model/item.go
@@ -3,10 +3,12 @@ package model
 import "github.com/zouipo/yumsday/backend/internal/model/enum"
 
 type Item struct {
-	ID                 int64              `json:"id"`
-	Name               string             `json:"name"`
-	Description        *string            `json:"description"`
-	AverageMarketPrice *float64           `json:"average_market_price"`
-	ItemCategory       *enum.ItemCategory `json:"item_category"`
-	UnitType           enum.UnitType      `json:"unit_type"`
+	ID                 int64         `json:"id"`
+	Name               string        `json:"name"`
+	Description        *string       `json:"description"`
+	AverageMarketPrice *float64      `json:"average_market_price"`
+	UnitType           enum.UnitType `json:"unit_type"`
+	ItemCategoryID     int64         `json:"item_category_id"`
+	GroupID            int64         `json:"group_id"`
+	Ingredients        []Ingredient  `json:"ingredients"`
 }
