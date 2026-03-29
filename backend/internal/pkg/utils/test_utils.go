4c9ad70183e65de4031fbffc2aeedab48895ab1d
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-03-29 16:30:58 +0200

    test_utils: add Ptr function

--- backend/internal/pkg/utils/test_utils.go
+++ backend/internal/pkg/utils/test_utils.go
@@ -8,6 +8,10 @@ import (
 	"github.com/mattn/go-sqlite3"
 )
 
+func Ptr[T any](v T) *T {
+	return &v
+}
+
 // TimesApproximatelyEqual checks if two time values are approximately equal within a specified tolerance.
 func TimesApproximatelyEqual(t1, t2 time.Time, tolerance time.Duration) bool {
 	diff := t1.Sub(t2)
