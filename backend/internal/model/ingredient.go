65a4bb8c9204f2902e676e53d5f47afdedb90977
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-04-04 18:34:36 +0200

    ingredient model: replace ItemID with Item

--- backend/internal/model/ingredient.go
+++ backend/internal/model/ingredient.go
@@ -3,7 +3,7 @@ package model
 type Ingredient struct {
 	ID       int64    `json:"id"`
 	Quantity *float64 `json:"quantity"`
+	Item     Item     `json:"item"`
 	RecipeID int64    `json:"recipe_id"`
-	ItemID   int64    `json:"item_id"`
 	UnitID   int64    `json:"unit_id"`
 }
