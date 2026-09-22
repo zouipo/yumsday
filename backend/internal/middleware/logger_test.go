29a44dfbd8136c56501cb294ef3d02b7ecc0598f
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-09-22 10:21:06 +0200

    test: test logger middleware

---
+++ backend/internal/middleware/logger_test.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type logHandler struct {
+	logged bool
+	record slog.Record
+}
+
+func newLogHandler() *logHandler {
+	return &logHandler{
+		logged: false,
+	}
+}
+
+func (l *logHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return true
+}
+
+func (l *logHandler) Handle(ctx context.Context, r slog.Record) error {
+	l.logged = true
+	l.record = r
+	return nil
+}
+
+func (l *logHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return l.WithAttrs(attrs)
+}
+
+func (l *logHandler) WithGroup(name string) slog.Handler {
+	return l.WithGroup(name)
+}
+
+type logReqHandler struct{}
+
+func (h *logReqHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestLogger(t *testing.T) {
+	logHandler := newLogHandler()
+	logger := slog.New(logHandler)
+	slog.SetDefault(logger)
+
+	mwStack := Stack(ResponseWriter, Logger)
+	handler := mwStack(&logReqHandler{})
+
+	r := httptest.NewRequest(http.MethodGet, "/test", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, r)
+
+	if !logHandler.logged {
+		t.Error("expected logger middleware to be called")
+	}
+}
