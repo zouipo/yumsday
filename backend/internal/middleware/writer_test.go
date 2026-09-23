c069c2f3d4c3ab28bac70a9a605ea5d80417075b
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-09-23 21:30:46 +0200

    test: remove useless body check in writer_test.go

--- backend/internal/middleware/writer_test.go
+++ backend/internal/middleware/writer_test.go
@@ -2,26 +2,19 @@ package middleware
 
 import (
 	"context"
-	"io"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 
 	"github.com/zouipo/yumsday/backend/internal/ctxkey"
 )
 
 type expectedStatusKey struct{}
-type testKey struct{}
 
 // final handler
 func writerReqHandler(w http.ResponseWriter, r *http.Request) {
 	status := r.Context().Value(expectedStatusKey{}).(int)
 	w.WriteHeader(status)
-
-	// write the status code as a string in the response body
-	statusStr := strconv.FormatInt(int64(status), 10)
-	io.WriteString(w, statusStr)
 }
 
 // middleware object used to extract the http status code
@@ -44,23 +37,11 @@ func performRequestAndCheck(t *testing.T, statusExtractor *statusExtractor, hand
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodGet, "/test", nil)
 	r = r.WithContext(context.WithValue(r.Context(), expectedStatusKey{}, expectedStatus))
-	r = r.WithContext(context.WithValue(r.Context(), testKey{}, t))
 
 	handler.ServeHTTP(w, r)
 
 	if statusExtractor.status != expectedStatus {
 		t.Errorf("expected status %v, got %v", expectedStatus, statusExtractor.status)
-	}
-
-	resp := w.Result()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Errorf("expected no error while reading response body, got %s", err)
-	}
-
-	expectedBody := strconv.FormatInt(int64(statusExtractor.status), 10)
-	if string(body) != expectedBody {
-		t.Errorf("expected body '%s', got '%s'", expectedBody, body)
 	}
 }
 
