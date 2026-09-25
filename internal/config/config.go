c5344d0b64c20f66811e3fa703971b1be0ebf64a
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2026-09-26 00:12:08 +0200

    feat: parse yaml config with goccy/go-yaml

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,48 +2,71 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	"log/slog"
 	"os"
 
-	"github.com/spf13/viper"
+	"github.com/goccy/go-yaml"
 )
 
 const (
-	CONFIG_PATH_ENV_VAR = "YUMSDAY_CONFIG_PATH"
+	configPathEnvVar = "YUMSDAY_CONFIG_PATH"
 )
 
 type Config struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	DBPath   string `mapstructure:"db_path"`
-	LogLevel string `mapstructure:"log_level"`
+	Host     string     `yaml:"host"`
+	Port     int        `yaml:"port"`
+	DBPath   string     `yaml:"db_path"`
+	LogLevel slog.Level `yaml:"log_level"`
 }
 
-func LoadConfig() (*Config, error) {
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(os.Getenv(CONFIG_PATH_ENV_VAR))
-	viper.AddConfigPath(".")
+func newConfig() Config {
+	return Config{
+		Host:     "[::0]",
+		Port:     8080,
+		DBPath:   "yumsday.db",
+		LogLevel: slog.LevelInfo,
+	}
+}
 
-	err := viper.ReadInConfig()
+func LoadConfig() (Config, error) {
+	yamlPath := configPath()
+
+	cfg, err := readConfigFile(yamlPath)
 	if err != nil {
-		if _, ok := errors.AsType[viper.ConfigFileNotFoundError](err); !ok {
-			return nil, err
+		return cfg, err
+	}
+
+	return cfg, nil
+}
+
+func configPath() string {
+	yamlPath := "./config.yaml"
+
+	if yamlPathEnvVar := os.Getenv(configPathEnvVar); yamlPathEnvVar != "" {
+		yamlPath = yamlPathEnvVar
+	}
+
+	return yamlPath
+}
+
+// readConfigFile parses the yaml config file and overwrite the default configuration with its content.
+// If the config file doesn't exist, then the default config is returned.
+func readConfigFile(yamlPath string) (Config, error) {
+	cfg := newConfig()
+
+	buf, err := os.ReadFile(yamlPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return cfg, nil
+		} else {
+			return cfg, fmt.Errorf("failed to read config file: %w", err)
 		}
 	}
 
-	viper.SetEnvPrefix("YUMSDAY")
-	// Check for environment variables following
-	// the pattern YUMSDAY_<viper>_<key>_<name>
-	// e.g. YUMSDAY_DB_PATH
-	viper.AutomaticEnv()
-
-	var config Config
-	// Assign values to the Config struct following the names
-	// given by the mapstructure attributes
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		return nil, err
+	if err := yaml.Unmarshal(buf, &cfg); err != nil {
+		return cfg, fmt.Errorf("failed to parse yaml config: %w", err)
 	}
 
-	return &config, nil
+	return cfg, nil
 }
